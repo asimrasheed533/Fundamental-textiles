7db4201f6500ac7605bc0cba033cfe1a8de9d938
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-05 17:48:56 +0500

    optimizations

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -88,7 +88,6 @@ export function ContactInfo() {
         <Fade bottom>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3402.8065460742096!2d74.37747681542659!3d31.474507381384846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391907a286809427%3A0x66f8b31237e79ada!2sDSME%20Global%20Links!5e0!3m2!1sen!2s!4v1662249400180!5m2!1sen!2s"
-            loading="lazy"
             className="contact__map"
             referrerpolicy="no-referrer-when-downgrade"
           />
